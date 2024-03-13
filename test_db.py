857e2562e02b6a91e6f395f57db6fd4c51974fac
kota iizuka <sexy.bugs0116@gmail.com>
2024-03-13 23:06:37 +0900

    a

--- test_db.py
+++ test_db.py
@@ -2,9 +2,7 @@ import os
 import psycopg2
 
 def get_connection():
-    dsn = os.environ.get('postgres://discord_bot_data_user:BdimL061db6iEusp0P8ftr4OnyyLj2Th@dpg-cnoph1vsc6pc73b7d3ug-a.oregon-postgres.render.com/discord_bot_data')
-    return psycopg2.connect("postgres://discord_bot_data_user:BdimL061db6iEusp0P8ftr4OnyyLj2Th@dpg-cnoph1vsc6pc73b7d3ug-a.oregon-postgres.render.com/discord_bot_data")
-
+    return
 
 with get_connection() as conn:
     with conn.cursor() as cur:
